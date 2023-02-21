4774887435abba6aa8cd74b356487f4e600da4ae
Dolgikh A.A <dolgikh355@gmail.com>
2023-02-21 21:31:29 +0400

    F-002

--- src/core/income/category/income-categories.service.ts
+++ src/core/income/category/income-categories.service.ts
@@ -20,24 +20,44 @@ export class IncomeCategoriesService {
       return await this.incomeCategoryRep.insert(income_category);
     } catch (error) {
       error.userError = 'Произошла ошибка при создании новой доходной категории.';
-      throw new HttpException(error, HttpStatus.NOT_FOUND);
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   async findAll(): Promise<IncomeCategory[]> {
-    return await this.incomeCategoryRep.find({ order: {order_pos: "ASC"} });
+    try {
+      return await this.incomeCategoryRep.find({ order: {order_pos: "ASC"} });
+    } catch (error) {
+      error.userError = 'Произошла ошибка при поиске доходных категорий.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async findOne(income_category_id: number): Promise<IncomeCategory> {
-    return await this.incomeCategoryRep.findOne({where: {income_category_id}});
+    try {
+      return await this.incomeCategoryRep.findOne({where: {income_category_id}});
+    } catch (error) {
+      error.userError = 'Произошла ошибка при поиске доходной категории.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async update(income_category_id: number, income_category: UpdateIncomeCategoryDto) {
-    return await this.incomeCategoryRep.update(income_category_id, income_category);
+    try {
+      return await this.incomeCategoryRep.update(income_category_id, income_category);
+    } catch (error) {
+      error.userError = 'Произошла ошибка при обновлении доходной категории.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async remove(income_category_id: number) {
-    return await this.incomeCategoryRep.delete(income_category_id);
+    try {
+      return await this.incomeCategoryRep.delete(income_category_id);
+    } catch (error) {
+      error.userError = 'Произошла ошибка при удалении доходной категории.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
 }
--- src/core/income/record/income-records.service.ts
+++ src/core/income/record/income-records.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository }       from 'typeorm';
 
@@ -17,30 +17,55 @@ export class IncomeRecordsService {
   ) {}
 
   async create(createIncomeRecordDto: CreateIncomeRecordDto) {
-    return await this.IncomeRecordRep.insert(createIncomeRecordDto);
+    try {
+      return await this.IncomeRecordRep.insert(createIncomeRecordDto);
+    } catch (error) {
+      error.userError = 'Произошла ошибка при создании записи о доходах.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async findAll() {
-    return await this.IncomeRecordRep.createQueryBuilder('ir')
-      .select(['ir.income_record_id as income_record_id',
-               'ii.nm_income_item   as nm_income_item',
-               'ii.income_item_id   as income_item_id',
-               'ir.summa            as summa',
-               'ir.description      as description'])
-      .leftJoin(IncomeItem, 'ii', 'ii.income_item_id = ir.income_item_id')
-      .orderBy({'ir.income_dt': 'ASC', 'ii.order_pos': 'ASC'})
-      .getRawMany()
+    try {
+      return await this.IncomeRecordRep.createQueryBuilder('ir')
+       .select(['ir.income_record_id as income_record_id',
+                'ii.nm_income_item   as nm_income_item',
+                'ii.income_item_id   as income_item_id',
+                'ir.summa            as summa',
+                'ir.description      as description'])
+        .leftJoin(IncomeItem, 'ii', 'ii.income_item_id = ir.income_item_id')
+        .orderBy({'ir.income_dt': 'ASC', 'ii.order_pos': 'ASC'})
+        .getRawMany()
+    } catch (error) {
+      error.userError = 'Произошла ошибка при поиске записей о доходах.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async findOne(income_record_id: number) {
-    return await this.IncomeRecordRep.findOne({where: {income_record_id}});
+    try {
+      return await this.IncomeRecordRep.findOne({where: {income_record_id}});
+    } catch (error) {
+      error.userError = 'Произошла ошибка при поиске записи о доходах.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async update(income_record_id: number, income_record: UpdateIncomeRecordDto) {
-    return await this.IncomeRecordRep.update(income_record_id, income_record);
+    try {
+      return await this.IncomeRecordRep.update(income_record_id, income_record);
+    } catch (error) {
+      error.userError = 'Произошла ошибка при обновлении записи о доходах.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async remove(income_record_id: number) {
-    return await this.IncomeRecordRep.delete(income_record_id);
+    try {
+      return await this.IncomeRecordRep.delete(income_record_id);
+    } catch (error) {
+      error.userError = 'Произошла ошибка при удалении записи о доходах.';
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
